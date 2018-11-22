928ec2c994f73715893166f7966f4b45d9efa0b9
Oleksandr Uvarov <oleksandr.uvarov@betlab.com>
2018-11-22 10:44:35 +0200

    [ADD] Create project

---
+++ Test_ask.fm/Test_ask.fm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_ask.fm
+//
+//  Created by Oleksandr Uvarov on 11/22/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
